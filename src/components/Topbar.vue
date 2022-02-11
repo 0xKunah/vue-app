41c4941012468fc9c5160789a436e33818e30cdc
Kunah <reply.kunah@gmail.com>
2022-02-11 18:23:50 +0100

    Initial commit

---
+++ src/components/Topbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div id="topbar" class='overflow-auto z-30 absolute top-0 w-full h-12 flex font-semibold text-gray-700 items-center px-4 bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400'>
+    <RouterLink to="/" class='ml-2 text-gray-200'>Home</RouterLink>
+
+    <div class="flex-grow"></div>
+    <RouterLink to="/hellow">Contact</RouterLink>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="w-full pt-12 h-full bg-gray-700">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </main>
+</template>
